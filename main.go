73783c51d9ed3c6ca9d7d12f48a68cbe62be5688
eben-vranken <vranken.eben@outlook.com>
2026-05-19 23:04:16 +0200

    feat: basic file serving + logging middleware

--- main.go
+++ main.go
@@ -7,21 +7,21 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/{file}", getFile)
 
-	log.Println("Listening on :8080")
-	log.Fatal(http.ListenAndServe("128.0.0.1:8080", nil))
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /", loggingMiddleware(http.FileServer(http.Dir("./public"))))
+
+	log.Println("Listening on :8080...")
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", mux))
 }
 
-func getFile(w http.ResponseWriter, req *http.Request) {
-	LoggingMiddleware()
-}
-
-func LoggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Println(req.URL.Path, "executing middleware")
 		start := time.Now()
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(w, req)
 		duration := time.Since(start)
-		log.Printf("[%s] %s %s", r.Method, r.RequestURI, duration)
+		log.Printf("[%s] %s %s", req.Method, req.RequestURI, duration)
 	})
 }
